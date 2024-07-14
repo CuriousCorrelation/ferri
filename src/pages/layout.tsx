7a581ed4b86febfb278be1c71d7efb2555dca1cf
CuriousCorrelation <CuriousCorrelation@gmail.com>
2024-07-14 18:39:56 +0530

    feat(app): Antialiased fonts

---
+++ src/pages/layout.tsx
@@ -0,0 +1,20 @@
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import { ReactElement } from "react"
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
+export default function Layout({ children }: { children: ReactElement }) {
+    return (
+        <main
+            className={cn(
+                "min-h-screen min-w-screen font-sans antialiased",
+                fontSans.variable
+            )}
+        >{children}</main>
+    )
+};
