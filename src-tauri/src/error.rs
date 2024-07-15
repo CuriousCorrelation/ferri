ec540f35c26ec9ed85d855c92a0ad25c85bfb691
CuriousCorrelation <CuriousCorrelation@gmail.com>
2024-07-15 20:40:29 +0530

    docs(server): File and zip errors

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,15 +1,22 @@
 use thiserror::Error;
 
+/// Custom error type for handling errors in ZIP archive operations.
 #[derive(Error, Debug)]
 pub(crate) enum FerriError {
+    /// Error encountered while opening a file.
     #[error("Failed to open file: {0}")]
     FileOpen(#[from] std::io::Error),
+
+    /// Error encountered while reading a ZIP archive.
     #[error("Failed to read ZIP archive: {0}")]
     Zip(#[from] zip::result::ZipError),
+
+    /// Error indicating an invalid password provided for decrypting a ZIP archive.
     #[error("Invalid password")]
     InvalidPassword(#[from] zip::result::InvalidPassword),
 }
 
+/// Conversion trait to convert `FerriError` into `tauri::InvokeError`.
 impl Into<tauri::InvokeError> for FerriError {
     fn into(self) -> tauri::InvokeError {
         match self {
@@ -24,4 +31,5 @@ impl Into<tauri::InvokeError> for FerriError {
     }
 }
 
+/// Result type alias for operations that may return `FerriError`.
 pub(crate) type FerriResult<T> = Result<T, FerriError>;
