090d8196968a53563b1f95bd3b303c2ec06991bf
CuriousCorrelation <CuriousCorrelation@gmail.com>
2024-07-15 20:39:16 +0530

    docs(server): Args and return documentation for models

--- src-tauri/src/model.rs
+++ src-tauri/src/model.rs
@@ -1,6 +1,11 @@
 use serde::Serialize;
 use serde_json::Value;
 
+/// Represents metadata for a ZIP archive.
+///
+/// Holds the overall metadata for a ZIP archive, including its name,
+/// total size, total compressed size, metadata for each file in the archive, and
+/// a hierarchical representation of the directory structure.
 #[derive(Serialize)]
 pub(crate) struct ZipArchiveMetadata {
     name: String,
@@ -11,6 +16,19 @@ pub(crate) struct ZipArchiveMetadata {
 }
 
 impl ZipArchiveMetadata {
+    /// Constructs a new `ZipArchiveMetadata`.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - The name of the ZIP archive.
+    /// * `archive_size` - The total size of the archive in a human-readable format.
+    /// * `archive_compressed_size` - The total compressed size of the archive in a human-readable format.
+    /// * `file_metadata` - A vector containing metadata for each file in the archive.
+    /// * `tree` - A JSON value representing the hierarchical directory structure of the archive.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `ZipArchiveMetadata`.
     pub(crate) fn new(
         name: String,
         archive_size: String,
@@ -28,6 +46,10 @@ impl ZipArchiveMetadata {
     }
 }
 
+/// Represents metadata for a file within a ZIP archive.
+///
+/// This structure holds the metadata for an individual file within a ZIP archive,
+/// including its name, size, and compressed size.
 #[derive(Serialize)]
 pub(crate) struct ZipFileMetadata {
     name: String,
@@ -36,6 +58,17 @@ pub(crate) struct ZipFileMetadata {
 }
 
 impl ZipFileMetadata {
+    /// Constructs a new `ZipFileMetadata`.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - The name of the file.
+    /// * `size` - The size of the file in a human-readable format.
+    /// * `compressed_size` - The compressed size of the file in a human-readable format.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `ZipFileMetadata`.
     pub(crate) fn new(
         name: String,
         size: String,
