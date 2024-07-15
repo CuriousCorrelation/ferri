6c55b5593ada48fd5624f0f8829cf9a42645c072
CuriousCorrelation <CuriousCorrelation@gmail.com>
2024-07-15 20:44:05 +0530

    docs(server): `docs.rs` crate README

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,59 @@
     windows_subsystem = "windows"
 )]
 
+/// # Ferri
+///
+/// **Ferri** is a zip file previewer written in [Rust](https://www.rust-lang.org/) and the
+/// [Tauri](https://tauri.app/) framework with a [React](https://reactjs.org/) and
+/// [TypeScript](https://www.typescriptlang.org/) front end. Ferri provides an efficient way
+/// to preview the contents of zip files without extracting them. It is designed to be fast,
+/// lightweight, and cross-platform.
+///
+/// ## Features
+///
+/// - Built with Rust and Tauri, Ferri runs efficiently on various operating systems
+///   including Windows, macOS, and Linux.
+/// - Reproducible development environment using [devenv](https://devenv.sh/) and
+///   [Nix](https://nixos.org/).
+/// - Minimal resource usage for quick load times and responsive UI.
+/// - Clean user interface built with React and TypeScript, styled using
+///   [TailwindCSS](https://tailwindcss.com/).
+///
+/// ## Screenshots
+///
+/// See the `screenshots` directory for visual representations of Ferri's features and user interface.
+///
+/// ## Development Environment
+///
+/// Ferri's development environment is powered by [devenv](https://devenv.sh/) and [Nix](https://nixos.org/).
+///
+/// ### Getting Started
+///
+/// 1. **Activate Developer Environment**: 
+///    ```sh
+///    devenv shell
+///    ```
+/// 2. **Start Processes**:
+///    ```sh
+///    devenv up
+///    ```
+///
+/// ### Prerequisites
+///
+/// Ensure you have the Nix package manager installed. Follow the [getting started guide](https://devenv.sh/getting-started/)
+/// if you need help installing Nix. Learn more about Nix and its benefits for creating reproducible, declarative,
+/// and reliable systems at [nixos.org](https://nixos.org/).
+///
+/// ### Notes
+///
+/// 1. Using `url: github:NixOS/nixpkgs/nixpkgs-unstable` instead of `url: github:cachix/devenv-nixpkgs/rolling` in `devenv.yaml`
+///    to resolve package version mismatches.
+/// 2. If you encounter screen artifacts due to version mismatches, run the app with:
+///    ```sh
+///    WEBKIT_DISABLE_COMPOSITING_MODE=1 pnpm dev
+///    ```
+///
+
 pub(crate) mod error;
 pub(crate) mod interop;
 pub(crate) mod model;
